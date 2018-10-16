71fa17e49287ee2755a1f96d0bf8d3e19075e2b5
Drew Sullivan <dsullivan@dev-030416.local>
2018-10-16 14:59:07 -0400

    First real commit

---
+++ CopyCat/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  CopyCat
+//
+//  Created by Drew Sullivan on 10/16/18.
+//  Copyright © 2018 Drew Sullivan, DMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func takePicture(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+        } else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+ 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        imageView.image = image
+        dismiss(animated: true, completion: nil)
+    }
+
+}
